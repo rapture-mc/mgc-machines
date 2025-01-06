7fa09aac3c065f15eb6af446d49441aebbf1bed9
benny@MGC-DRS-01 <ben.r.harris@protonmail.com>
2025-01-06 14:40:14 +0930

    replacing deploy user with controller

--- machines/hypervisors/MGC-DRW-HVS01/deploy.nix
+++ machines/hypervisors/MGC-DRW-HVS01/deploy.nix
@@ -6,7 +6,7 @@
 }: {
   hostname = "${vars.networking.hostsAddr.MGC-DRW-HVS01.ipv4}";
   profiles.system = {
-    sshUser = "deploy";
+    sshUser = "controller";
     user = "root";
     path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.MGC-DRW-HVS01;
     sshOpts = ["-i" "/var/lib/gitea-runner/default/.ssh/id_ed25519"];
