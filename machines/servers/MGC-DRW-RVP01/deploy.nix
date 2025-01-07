79466f7ed0759031444d18cb162d1c2f8c26bba9
Ben Harris <ben.r.harris@protonmail.com>
2025-01-07 12:19:32 +0930

    should be MGC-DRW-RVP01 not MGC-RVP-01

--- machines/servers/MGC-DRW-RVP01/deploy.nix
+++ machines/servers/MGC-DRW-RVP01/deploy.nix
@@ -4,11 +4,11 @@
   vars,
   ...
 }: {
-  hostname = "${vars.networking.hostsAddr.MGC-RVP-01.ipv4}";
+  hostname = "${vars.networking.hostsAddr.MGC-DRW-RVP01.ipv4}";
   profiles.system = {
     sshUser = "controller";
     user = "root";
-    path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.MGC-RVP-01;
+    path = deploy-rs.lib.x86_64-linux.activate.nixos self.nixosConfigurations.MGC-DRW-RVP01;
     remoteBuild = true;
   };
 }
