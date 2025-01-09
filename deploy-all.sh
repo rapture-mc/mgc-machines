96abd30d8252a1caa8ed5fa400d2d950f51cc584
Ben Harris <ben.r.harris@protonmail.com>
2025-01-09 14:21:06 +0930

    adding deploy script

---
+++ deploy-all.sh
@@ -0,0 +1,9 @@
+#!/run/current-system/sw/bin/bash
+
+deploy .#MGC-DRW-HVS01 -s
+deploy .#MGC-DRW-BST01 -s
+deploy .#MGC-DRW-GUC01 -s
+deploy .#MGC-DRW-HDS01 -s
+deploy .#MGC-DRW-PWS01 -s
+deploy .#MGC-DRW-RST01 -s
+deploy .#MGC-DRW-RVP01 -s
