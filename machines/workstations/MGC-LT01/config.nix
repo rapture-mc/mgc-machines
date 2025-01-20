827dd8433445a0cc6c85bb30b47dc6aa73e84209
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-01-20 18:54:58 +0930

    removing extra stuff

--- machines/workstations/MGC-LT01/config.nix
+++ machines/workstations/MGC-LT01/config.nix
@@ -12,31 +12,6 @@ nixpkgs.lib.nixosSystem {
         ./hardware-config.nix
       ];
 
-      networking.firewall = {
-        allowedUDPPorts = [ 51820 ];
-      };
-
-      # networking = {
-      #   wireguard = {
-      #     enable = true;
-      #     interfaces = {
-      #       wg0 = {
-      #         ips = [ "10.100.0.2/24" ];
-      #         listenPort = 51820;
-      #         privateKeyFile = "/home/benny/wireguard-keys/private";
-      #         peers = [
-      #           {
-      #             publicKey = "CthODhxdRHTxTAdqcRlqYAYkRBXMuDFA7AkQfyIrTxY=";
-      #             allowedIPs = [ "10.100.0.0/24" ];
-      #             endpoint = "123.243.147.17:51820";
-      #             persistentKeepalive = 25;
-      #           }
-      #         ];
-      #       };
-      #     };
-      #   };
-      # };
-
       environment.systemPackages = with nixpkgs.legacyPackages.x86_64-linux; [
         go
         gnucash
@@ -60,13 +35,13 @@ nixpkgs.lib.nixosSystem {
           wireguard-client = {
             enable = true;
             private-key-file = "/home/benny/wireguard-keys/private";
-            ipv4 = "${vars.networking.hostsAddr.MGC-LT01.wireguard.ipv4}";
             server = {
               ipv4 = "${vars.networking.wireguardPublicIP}";
               public-key = "${vars.wireguardPubKeys.MGC-DRW-VPN01}";
             };
           };
         };
+
         virtualisation.whonix.enable = true;
       };
     }
