a33e38ca9f3461b1df639efb7b3031b0f0105cad
benny@MGC-DRW-CTR01 <ben.r.harris@protonmail.com>
2025-02-17 17:23:25 +0930

    adding pws01

--- infra/MGC-DRW-HVS02/terranix.nix
+++ infra/MGC-DRW-HVS02/terranix.nix
@@ -11,8 +11,8 @@
   terraformConfiguration = terranix.lib.terranixConfiguration {
     inherit system;
     modules = [
-      (import ./virtual-machines/MGC-DRW-BST01.nix {inherit vars;})
-      # (import ./virtual-machines/MGC-DRW-PWS01.nix {inherit vars;})
+      # (import ./virtual-machines/MGC-DRW-BST01.nix {inherit vars;})
+      (import ./virtual-machines/MGC-DRW-PWS01.nix {inherit vars;})
       # (import ./virtual-machines/MGC-DRW-RST01.nix {inherit vars;})
     ];
   };
