2c43bbbcaf03c3fa8131feaeeefc728c142cde19
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-02-21 11:23:30 +0930

    opening 1313

--- machines/hypervisors/MGC-DRW-HVS02/config.nix
+++ machines/hypervisors/MGC-DRW-HVS02/config.nix
@@ -30,6 +30,7 @@ nixpkgs.lib.nixosSystem {
 
       # Hugo stuff
       environment.systemPackages = [nixpkgs.legacyPackages.x86_64-linux.hugo];
+      networking.firewall.allowedTCPPorts = [ 1313 ];
 
       megacorp = {
         config = {
