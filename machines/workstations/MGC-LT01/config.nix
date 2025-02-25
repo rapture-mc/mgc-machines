5cec4b634993c9697102345a5da22e18873d24ba
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-02-25 18:33:13 +0930

    setting branch to staging

--- machines/workstations/MGC-LT01/config.nix
+++ machines/workstations/MGC-LT01/config.nix
@@ -52,6 +52,7 @@ nixpkgs.lib.nixosSystem {
           comin = {
             enable = true;
             repo = "https://github.com/rapture-mc/mgc-machines";
+            branch = "staging";
           };
         };
 
