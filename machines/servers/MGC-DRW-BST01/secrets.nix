eea41cef2c35d1136384d0c6f26e3ca42a367725
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-04-10 11:52:12 +0930

    updating sops keys

--- machines/servers/MGC-DRW-BST01/secrets.nix
+++ machines/servers/MGC-DRW-BST01/secrets.nix
@@ -4,7 +4,7 @@
   sops = {
     defaultSopsFile = ../../../sops/secrets/default.yaml;
     defaultSopsFormat = "yaml";
-    gnupg.sshKeyPaths = ["/home/${vars.adminUser}/.config/sops/age/keys.txt"];
+    age.keyFile = "/home/${vars.adminUser}/.config/sops/age/keys.txt";
     secrets.restic-repo-password = {};
   };
 }
