6193e57e4dae142a76c043d398f50f49d60913d7
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-05-25 14:21:07 +0930

    adding nameserver

--- machines/servers/MGC-DRW-BST01/config.nix
+++ machines/servers/MGC-DRW-BST01/config.nix
@@ -27,6 +27,7 @@ nixpkgs.lib.nixosSystem {
             ipv4 = vars.networking.hostsAddr.MGC-DRW-BST01.eth.ipv4;
             interface = vars.networking.hostsAddr.MGC-DRW-BST01.eth.name;
             gateway = vars.networking.defaultGateway;
+            nameservers = vars.networking.nameServers;
             lan-domain = vars.networking.internalDomain;
           };
 
