1e6cd910a8dd800333105b5036f7cad7b2c7dabb
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-05-25 14:16:52 +0930

    removing dns service from BST01

--- machines/servers/MGC-DRW-BST01/config.nix
+++ machines/servers/MGC-DRW-BST01/config.nix
@@ -16,26 +16,7 @@ nixpkgs.lib.nixosSystem {
         ./hardware-config.nix
       ];
 
-      networking = {
-        hostName = "MGC-DRW-BST01";
-
-        hosts = {
-          "${vars.networking.hostsAddr.MGC-DRW-CTR01.eth.ipv4}" = ["MGC-DRW-CTR01"];
-          "${vars.networking.hostsAddr.MGC-DRW-GRF01.eth.ipv4}" = ["MGC-DRW-GRF01"];
-          "${vars.networking.hostsAddr.MGC-DRW-GUC01.eth.ipv4}" = ["MGC-DRW-GUC01"];
-          "${vars.networking.hostsAddr.MGC-DRW-PWS01.eth.ipv4}" = ["MGC-DRW-PWS01"];
-          "${vars.networking.hostsAddr.MGC-DRW-RST01.eth.ipv4}" = ["MGC-DRW-RST01"];
-          "${vars.networking.hostsAddr.MGC-DRW-FBR01.eth.ipv4}" = ["MGC-DRW-FBR01"];
-          "${vars.networking.hostsAddr.MGC-DRW-RVP01.eth.ipv4}" = ["MGC-DRW-RVP01"];
-          "${vars.networking.hostsAddr.MGC-DRW-SEM01.eth.ipv4}" = ["MGC-DRW-SEM01"];
-          "${vars.networking.hostsAddr.MGC-DRW-HVS01.eth.ipv4}" = ["MGC-DRW-HVS01"];
-          "${vars.networking.hostsAddr.MGC-DRW-HVS02.eth.ipv4}" = ["MGC-DRW-HVS02"];
-          "${vars.networking.hostsAddr.MGC-DRW-HVS03.eth.ipv4}" = ["MGC-DRW-HVS03"];
-          "${vars.networking.hostsAddr.MGC-DRW-DMC01.eth.ipv4}" = ["MGC-DRW-DMC01"];
-          "${vars.networking.hostsAddr.MGC-DRW-ZBX01.eth.ipv4}" = ["MGC-DRW-ZBX01"];
-          "192.168.1.99" = ["MGC-DRW-FRW01"];
-        };
-      };
+      networking.hostName = "MGC-DRW-BST01";
 
       system.stateVersion = "24.11";
 
@@ -63,11 +44,6 @@ nixpkgs.lib.nixosSystem {
           comin = {
             enable = true;
             repo = "https://github.com/rapture-mc/mgc-machines";
-          };
-
-          dnsmasq = {
-            enable = true;
-            domain = vars.networking.internalDomain;
           };
         };
       };
