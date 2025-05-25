767b56c50a63c83a26bc290eebc35894fd226f00
benny@MC-LT-01 <ben.r.harris@protonmail.com>
2025-05-25 19:52:15 +0930

    deleting infra directory

--- infra/default.nix
+++
@@ -1,7 +0,0 @@
-{importTerraformConfig}: {
-  MGC-DRW-HVS01-apply = importTerraformConfig "MGC-DRW-HVS01" "apply";
-  MGC-DRW-HVS01-destroy = importTerraformConfig "MGC-DRW-HVS01" "destroy";
-
-  MGC-DRW-HVS02-apply = importTerraformConfig "MGC-DRW-HVS02" "apply";
-  MGC-DRW-HVS02-destroy = importTerraformConfig "MGC-DRW-HVS02" "destroy";
-}
