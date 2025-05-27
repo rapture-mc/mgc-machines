2589ed2d739ad11c31b85a6c9271deebd87f08c0
benny@MGC-DRW-HVS01 <ben.r.harris@protonmail.com>
2025-05-27 12:28:04 +0930

    using new megacorp.dnsmasq.hosts option

--- machines/servers/MGC-DRW-DMC01/config.nix
+++ machines/servers/MGC-DRW-DMC01/config.nix
@@ -45,6 +45,24 @@ in nixpkgs.lib.nixosSystem {
             repo = "https://github.com/rapture-mc/mgc-machines";
           };
 
+          dnsmasq = {
+            enable = true;
+            domain = vars.networking.internalDomain;
+            hosts = ''
+              ${vars.networking.hostsAddr.MGC-DRW-DGW01.eth.ipv4} MGC-DRW-DGW01
+              ${vars.networking.hostsAddr.MGC-DRW-BST01.eth.ipv4} MGC-DRW-BST01
+              ${vars.networking.hostsAddr.MGC-DRW-PWS01.eth.ipv4} MGC-DRW-PWS01
+              ${vars.networking.hostsAddr.MGC-DRW-RST01.eth.ipv4} MGC-DRW-RST01
+              ${vars.networking.hostsAddr.MGC-DRW-RVP01.eth.ipv4} MGC-DRW-RVP01
+              ${vars.networking.hostsAddr.MGC-DRW-HVS01.eth.ipv4} MGC-DRW-HVS01
+              ${vars.networking.hostsAddr.MGC-DRW-HVS02.eth.ipv4} MGC-DRW-HVS02
+              ${vars.networking.hostsAddr.MGC-DRW-HVS03.eth.ipv4} MGC-DRW-HVS03
+              ${vars.networking.hostsAddr.MGC-DRW-DMC01.eth.ipv4} MGC-DRW-DMC01
+              ${vars.networking.hostsAddr.MGC-DRW-FBR01.eth.ipv4} MGC-DRW-FBR01
+              192.168.1.99 MGC-DRW-FRW01
+            '';
+          };
+
           openldap = {
             enable = true;
             domain-component = domain-component;
